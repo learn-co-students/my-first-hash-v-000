74c78da394795bc4708e20f8331fee70fa83f9fd
Richard-Frost <frost.rchrd@gmail.com>
2015-12-25 13:20:48 -0800

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,13 +1,8 @@
 def my_hash
   my_hash = {"Ruby" => "awesome!"}
-  
 end
 
-
-def shipping_manifest
-  #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+def shipping_manifest 
 shipping_manifest = {
   "whale bone corsets" => 5, 
   "porcelain vases" => 2, 
@@ -21,9 +16,6 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
-  #your code here
-  #remember to return the shipping_manifest hash
   shipping_manifest["pearl necklace"] = 1
   shipping_manifest
 end
