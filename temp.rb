c5d3eaa9afd479d4086b85dab2496602a2ebe317
smithWEBtek <brad@smithwebtek.com>
2016-01-25 04:49:55 -0500

    Done.

--- temp.rb
+++ temp.rb
@@ -1,40 +1,13 @@
 
 
-shipping_manifest = Hash.new
-shipping_mannifest = ["oil paintings"]=> 3, 
-["pearl necklace"]=>1, 
-["porcelain vases"]=>2, 
-["whale bone corsets"]=>5
-
-
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = ["eggs"]=> 12, ["coffee"]=>1, ["milk"]=>2, ["apples"]=>5
-end
-
-
-def shipping_manifest
-  #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
-end
-
-def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
-end
-
-def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  # my_hash = {Name:"Brad", Phone:"603-494-4147", ST:"NH", Zip:"03110"}
+  # puts my_hash
+  # puts "----------------"
   
-  #your code here
-  #remember to return the shipping_manifest hash
-
+  new_hash = {key1: "value1", key2: "value2"}
+  hash.each do |key, value| puts "#{key}: #{value}"
+end
 end
 
-
-
-
+my_hash
