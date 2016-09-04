550c9c30dda0bc1ede34c7c355ea18f0628b07c2
smithWEBtek <brad@smithwebtek.com>
2016-09-04 15:15:21 -0400

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,14 +9,11 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  return shipping_manifest["oil paintings"]
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["pearl necklace"]=1
-  return shipping_manifest
+  shipping_manifest
 end
-
-
-
--- temp.rb
+++ temp.rb
@@ -1,12 +1,12 @@
 
 
 def my_hash
-  # my_hash = {Name:"Brad", Phone:"603-494-4147", ST:"NH", Zip:"03110"}
-  # puts my_hash
-  # puts "----------------"
+  my_hash = {Name:"Brad", Phone:"603-494-4147", ST:"NH", Zip:"03110"}
+  puts my_hash
+  puts "----------------"
   
   new_hash = {key1: "value1", key2: "value2"}
-  hash.each do |key, value| puts "#{key}: #{value}"
+  new_hash.each do |key, value| puts "#{key}: #{value}"
 end
 end
 
