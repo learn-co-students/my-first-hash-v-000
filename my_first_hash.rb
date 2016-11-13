7727dd1a91ad4235c1dd0ae2af4e9a1c4d2e4aed
Elias Ventura <eliasventura23@gmail.com>
2016-11-13 21:13:49 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"my_name" => "Elias Ventura"}
+  my_hash = {"my name" => "Elias Ventura"}
+
 end
 
 
@@ -10,6 +11,7 @@ def shipping_manifest
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
 end
 
 def retrieval
