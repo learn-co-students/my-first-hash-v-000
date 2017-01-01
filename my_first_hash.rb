7caeccb73bf4dcb405de4c4ee5ce59ed6b411195
charlie lee <ctlee9096@gmail.com>
2017-01-01 00:18:14 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 my_hash = {"my_name" =>"Charlie Lee"}
@@ -12,7 +13,8 @@ end
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   #your code here!
-  hash["oil paintings"]
+  binding.pry
+  shipping_manifest["oil paintings"]
 end
 
 def adding
