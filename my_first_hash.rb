828bff801d63c39fe09f238c44c35352df735978
Rebecca Bratu <beckybratu@gmail.com>
2017-04-20 02:32:43 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,22 +7,16 @@ def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-  shipping_manifest = {}
-  shipping_manifest["whale bone corsets"] = 5
-  shipping_manifest["porcelain vases"] = 2
-  shipping_manifest["oil paintings"] = 3
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here
-  #remember to return the shipping_manifest hash
-
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest 
 end
