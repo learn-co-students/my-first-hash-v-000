18268a34ff06b440441d4d76bc089873a47fa19f
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-04 23:02:21 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,20 +1,18 @@
 def my_hash
-my_name = {"name" => "Sam I Am"}
-
+  your_hash = {"name" => "Steve"}
 end
 
-
 def shipping_manifest
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["oil paintings"]
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["pearl necklace"] = 1
-  shipping_manifest
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest["pearl necklace"] = 1
+  the_manifest
 end
