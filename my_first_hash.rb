c7ade5412b4cd35cc7bd91f49fb783ebc98227a6
Harleigh Abel <harleighabel@gmail.com>
2018-02-08 03:47:11 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,18 +1,17 @@
 def my_hash
-  hash = {"user" => "Harleigh","laptop" => "Dell XPS 15z", "desktop" => "IMac 27 inch", "tablet" => "Samsung Galaxy Tab e", "smartphone" => "Blu Advance 5.0"}
+my_hash = {"Rocket" => "cat", "George" => "fish"}
 end
 
 def shipping_manifest
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-end
-
+  shipping_manifest = {"whale bone corsets" => 2, "oil paintings" => 3, "porcelain vases" => 5}
+end  
 def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["oil paintings"]
-end
-
+end  
+  
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest["pearl necklace"] = 1 
   shipping_manifest
-end
+end  
+  
