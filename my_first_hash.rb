9c772b1146bae5da655329c0f5f08bfb002d2d0e
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-26 16:29:13 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,38 +1,40 @@
 def my_hash
- your_hash = {"name" => "Billy", "profession" => "Advisor"}
-  
+ your_hash = {"name" => "Billy", "position" => "Software Engineer"}
 end
 
+puts my_hash
 
 def shipping_manifest
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                  }
-
+  the_manifest = {
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2,
+    "oil paintings" => 3 
+  }
 end
+
+puts shipping_manifest
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  shipping_manifest["oil paintings"]
+  the_manifest = {
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2,
+    "oil paintings" => 3 
+  }
+ the_manifest["oil paintings"]
 end
+
+puts retrieval
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  shipping_manifest["muskets"] = 2
-
-  shipping_manifest["gun powder"] = 4
-
-  shipping_manifest
+  the_manifest = {
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2,
+    "oil paintings" => 3 
+  }
+  
+  the_manifest["muskets"] = 2
+  the_manifest["gun powder"] =4
+  the_manifest
 end
+
+puts adding 
