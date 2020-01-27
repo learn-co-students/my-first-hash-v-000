a92662e0ac92b5e833511345a539fc7239fdb2d6
Julie  McManus <julieannesays@gmail.com>
2020-01-27 22:13:24 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,24 +1,19 @@
+
 def my_hash
-	vacation_clothing = {"dresses" => 4, "bathing suit" => 2, "sandals" => 3}  
+	your_hash = { "key" => "value", "another_key" => "another_value"}
 end
 
-
-def shipping_manifest 
-	shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+def shipping_manifest
+	shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
-
 
 def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-	shipping_manifest["oil paintings"] 
+	shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+	shipping_manifest["oil paintings"]
 end
-
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["pearl necklace"] = 1
-  shipping_manifest
+	shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+	shipping_manifest["pearl necklace"] = 1
+	shipping_manifest
 end
-
-
-
